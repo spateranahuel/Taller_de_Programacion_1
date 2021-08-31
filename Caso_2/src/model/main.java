8b4a5182a570aa9a0dab55108904381b8b6f376a
ehererik <59492507+ehererik@users.noreply.github.com>
2021-08-31 17:38:35 -0300

    prueba 2

--- Caso_2/src/model/main.java
+++ Caso_2/src/model/main.java
@@ -1,9 +1,11 @@
 package model;
 
+import jdk.internal.misc.FileSystemOption;
+
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
 
 	}
 
